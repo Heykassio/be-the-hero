2970e55a1794d19e49591339931663531035b679
unknown <heykassio@live.com>
2020-03-28 16:32:49 -0300

    mobile da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -31,7 +31,7 @@ export default function Register(){
         const response = await api.post('ongs', data);
         alert(`Seu ID de acesso: ${response.data.id}`);
         history.push('/');
-        
+
        }catch(err){
            alert('Erro no cadastro, tente novamente.');
        }
